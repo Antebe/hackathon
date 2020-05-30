49d1b14bcdb36ee21d0162ce7001cfe5c5cbb687
Antebe <54448351+Antebe@users.noreply.github.com>
2020-05-30 20:37:43 +0300

    + графік

---
+++ src/com/company/Data.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Data {
+    int[] calendar;
+    double[] gas;
+    public int[] getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(int[] calendar) {
+        this.calendar = calendar;
+    }
+
+    public double[] getGas() {
+        return gas;
+    }
+
+    public void setGas(double[] gas) {
+        this.gas = gas;
+    }
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,22 @@
 package com.company;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import jdk.nashorn.internal.runtime.ListAdapter;
 
+public class Main extends Application {
 
-    public static void main(String[] args) {
-
+   public static Data dataseter = new Data();
+public static void main(String[] args) {
 
 
 //ініціалізуємо датасет випадків ковід 19
@@ -24,11 +28,11 @@ public class Main {
         int[] deaths = new int[sizelist];
         String[] country = new String[sizelist];
         String[] continent = new String[sizelist];
-    for (int i = 1; i < c19cases.size(); i++) {
-        String[] strings = c19cases.get(i);
+        for (int i = 1; i < c19cases.size(); i++) {
+            String[] strings = c19cases.get(i);
             for (int j = 0; j < strings.length; j++) {
                 //System.out.print(strings[j] + " ");
-                switch (j){
+                switch (j) {
                     case 1:
                         day[i] = Integer.parseInt(strings[j]);
                         break;
@@ -50,18 +54,18 @@ public class Main {
                 }
 
             }
-           // System.out.println();
+            // System.out.println();
         }
 
-    //ксть смертей в Україні
+        //ксть смертей в Україні
         int deathTotalUkr = 0;
         for (int i = 1; i < sizelist; i++) {
-            if(country[i].equals("Ukraine") ){
+            if (country[i].equals("Ukraine")) {
                 deathTotalUkr = deaths[i] + deathTotalUkr;
                 System.out.println(day[i] + "/" + month[i] + " " + deaths[i]);
             }
         }
-        System.out.println("В Україні померло- "+ deathTotalUkr);
+        System.out.println("В Україні померло- " + deathTotalUkr);
 
 //ініціалізуємо датасет якості повітря
         List<String[]> air = get("waqi-covid19-airqualitydata-2020.csv");
@@ -71,6 +75,7 @@ public class Main {
         int[] airmonths = new int[sizea];
         int[] airyear = new int[sizea];
         String[] aircountrycode = new String[sizea];
+        String[] city = new String[sizea];
         String[] airgas = new String[sizea];
         double[] airmax = new double[sizea];
         for (int i = 5; i < air.size(); i++) {
@@ -86,8 +91,10 @@ public class Main {
                         airyear[i] = Integer.parseInt(dates[0]);
                         break;
                     case 1:
-                        aircountrycode[i] =(strings[j]);
+                        aircountrycode[i] = (strings[j]);
                         break;
+                    case 2:
+                        city[i] = strings[j];
                     case 3:
                         airgas[i] = (strings[j]);
                         break;
@@ -99,18 +106,25 @@ public class Main {
             }
         }
 
-        //озон в Бельгії до і після
-        for (int i = 5; i < sizea ; i++) {
-            if (aircountrycode[i].equals("BE") && airgas[i].equals("o3") && airdays[i] == 11 && airmonths[i] == 1){
-                System.out.println("BEFORE - " + airdates[i] + " " + airgas[i] + " " + airmax[i] );
+        //Київ
+       List<Double> data = new ArrayList<Double>();
+       List<Integer> calendar = new ArrayList<Integer>();;
+        for (int i = 5; i < sizea; i++) {
+            if (aircountrycode[i].equals("UA") && airgas[i].equals("pm25")  && city[i].equals("Kyiv")) {
+                System.out.println("Current - " + airdates[i] + " " + airgas[i] + " " + airmax[i]);
+                data.add(airmax[i]);
+                calendar.add(newyear(airmonths[i], airdays[i]));//типу дні після Нового року
             }
         }
-        for (int i = 5; i < sizea ; i++) {
-            if (aircountrycode[i].equals("BE") && airgas[i].equals("o3") && airdays[i] == 16 && airmonths[i] == 5 ){
-                System.out.println("NOW - " + airdates[i] + " " + airgas[i] + " " + airmax[i] );
-            }
-        }
+        //сортуємо
+        multiSort(calendar, calendar, data);
+        double[] dataarr = data.stream().mapToDouble(d -> d).toArray();
+        int[] calarr = calendar.stream().mapToInt(d -> d).toArray();
+        System.out.println("res " + dataarr[0] + " " + calarr[0]);
+        dataseter.setCalendar(calarr);
+        dataseter.setGas(dataarr);
 
+        Application.launch(args);
 
 
     }
@@ -120,6 +134,44 @@ public class Main {
 
 
 
+
+
+    public static <T extends Comparable<T>> void multiSort(
+            final List<T> key, List<?>... lists){
+        // Create a List of indices
+        List<Integer> indices = new ArrayList<Integer>();
+        for(int i = 0; i < key.size(); i++) {
+            indices.add(i);
+        }
+
+        // Sort the indices list based on the key
+        Collections.sort(indices, new Comparator<Integer>() {
+            @Override public int compare(Integer i, Integer j) {
+                return key.get(i).compareTo(key.get(j));
+            }
+        });
+
+        // Create a mapping that allows sorting of the List by N swaps.
+        // Only swaps can be used since we do not know the type of the lists
+        Map<Integer,Integer> swapMap = new HashMap<Integer, Integer>(indices.size());
+        List<Integer> swapFrom = new ArrayList<Integer>(indices.size()),
+                swapTo   = new ArrayList<Integer>(indices.size());
+        for (int i = 0; i < key.size(); i++) {
+            int k = indices.get(i);
+            while (i != k && swapMap.containsKey(k)) {
+                k = swapMap.get(k);
+            }
+
+            swapFrom.add(i);
+            swapTo.add(k);
+            swapMap.put(i, k);
+        }
+
+        // use the swap order to sort each list by swapping elements
+        for (List<?> list : lists)
+            for (int i = 0; i < list.size(); i++)
+                Collections.swap(list, swapFrom.get(i), swapTo.get(i));
+    }
 
 
 //    public static int longi(double l, int w){
@@ -153,5 +205,42 @@ public class Main {
         return data;
     }
 
+    public static int newyear(int x, int y){
+        if(x==12){
+            x = -y;
+        }else {
+            x = 30*(x-1) + y;
+        }
+        return x;
+    }
 
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        NumberAxis xAxis = new NumberAxis();
+        xAxis.setLabel("days after the New Year");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("PM 2.5 in Kyiv");
+
+        LineChart lineChart = new LineChart(xAxis, yAxis);
+
+        XYChart.Series dataSeries1 = new XYChart.Series();
+        dataSeries1.setName("pm25");
+
+        for (int i = 0; i < dataseter.getCalendar().length; i++) {
+            dataSeries1.getData().add(new XYChart.Data(dataseter.getCalendar()[i], dataseter.getGas()[i]));
+            System.out.println(dataseter.getCalendar()[i] + " - " + dataseter.getGas()[i]);
+        }
+        lineChart.getData().add(dataSeries1);
+        VBox vbox = new VBox(lineChart);
+        Scene scene = new Scene(vbox, 400, 200);
+
+        primaryStage.setScene(scene);
+        primaryStage.setHeight(300);
+        primaryStage.setWidth(1200);
+
+        primaryStage.show();
+
+    }
 }
